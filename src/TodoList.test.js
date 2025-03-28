3732ad574d399e8b0127889ce6ade9d58228eedc
Bakhtiar Ismail <bakhtiar.khiderismail@edu.turkuamk.fi>
2025-03-28 13:03:42 +0200

    Update

--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { TodoList } from './TodoList';
 
 it('renders No task found when the list is empty', () => {
-  render(<TodoList />);
+  render(<TodoList list={[]} remove={() => {}} />);
   const linkElement = screen.getByText(/No task found/i);
   expect(linkElement).toBeInTheDocument();
 });
